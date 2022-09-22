06c1fb227aef9a780bae7321853a3b91ac6d3f96
Austin <austinccy81@gmail.com>
2022-09-22 21:50:02 +0100

    seeded database, added views with tailwinds

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,26 +1,11 @@
 <?php
-  namespace App\Models;
-  class Listing{
-    public static function all(){
-      return [
-        [
-          'id' => 1,
-          'title' => 'Listing One',
-          'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis augue nec est gravida blandit at et tellus. Phasellus molestie ornare maximus. In eget tellus faucibus, blandit risus in, varius.'
-      ],
-      [
-          'id' => 2,
-          'title' => 'Listing Two',
-          'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis augue nec est gravida blandit at et tellus. Phasellus molestie ornare maximus. In eget tellus faucibus, blandit risus in, varius.'
-      ]
-      ];
-    }
-    public static function find($id){
-      $listings = self::all();
-      foreach($listings as $listing){
-        if($listing['id']==$id){
-          return $listing;
-        }
-      }
-    }
-  }
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         Listing::create([
+            'title' => 'Yoga Trainer',
+            'tags' => 'yoga',
+            'company' => 'none',
+            'location' => 'Nottingham',
+            'email' => 'email@email.com',
+            'website' => 'https://www.google.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula libero sit amet diam tempus, eget pretium justo scelerisque. Sed a pretium enim, et vestibulum justo. Etiam sodales nulla sit.',
+            ] 
+        );
+        Listing::create([
+            'title' => 'HIIT Trainer',
+            'tags' => 'hiit',
+            'company' => 'none',
+            'location' => 'London',
+            'email' => 'test@test.com',
+            'website' => 'https://www.google.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula libero sit amet diam tempus, eget pretium justo scelerisque. Sed a pretium enim, et vestibulum justo. Etiam sodales nulla sit.',
+            ] 
+        );
     }
 }
