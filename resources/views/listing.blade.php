e72a4983055dd749ba439b08d7c464780910acd6
Austin <austinccy81@gmail.com>
2022-09-23 13:42:07 +0100

    added components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,11 +1,75 @@
 @extends('layout')
 @section('content')
 @include('partials._search')
-<h2>
-  {{$listing['title']}}
-</h2>
-<p>
-  {{$listing['description']}}
-</p>
+<a href="/" class="inline-block text-black ml-4 mb-4"
+                ><i class="fa-solid fa-arrow-left"></i> Back
+            </a>
+            <div class="mx-4">
+                <x-card  class="p-10">
+                    <div
+                        class="flex flex-col items-center justify-center text-center"
+                    >
+                        <img
+                            class="w-48 mr-6 mb-6"
+                            src="{{asset('images/no-image.png')}}"
+                            alt=""
+                        />
+
+                        <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
+                        <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                        <ul class="flex">
+                            <li
+                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                            >
+                                <a href="#">Yoga</a>
+                            </li>
+                            <li
+                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                            >
+                                <a href="#">HIIT</a>
+                            </li>
+                            <li
+                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                            >
+                                <a href="#">Fat burn</a>
+                            </li>
+                            <li
+                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
+                            >
+                                <a href="#">Toning</a>
+                            </li>
+                        </ul>
+                        <div class="text-lg my-4">
+                            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                        </div>
+                        <div class="border border-gray-200 w-full mb-6"></div>
+                        <div>
+                            <h3 class="text-3xl font-bold mb-4">
+                                Description
+                            </h3>
+                            <div class="text-lg space-y-6">
+                                <p>
+                                  {{$listing->description}}
+                                </p>
+
+                                <a
+                                    href="mailto:{{$listing->email}}"
+                                    class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
+                                    ><i class="fa-solid fa-envelope"></i>
+                                    Contact Trainer</a
+                                >
+
+                                <a
+                                    href="{{$listing->website}}"
+                                    target="_blank"
+                                    class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
+                                    ><i class="fa-solid fa-globe"></i> Visit
+                                    Website</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+                </x-card>
+            </div>
 
 @endsection
